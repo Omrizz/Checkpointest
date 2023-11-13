b9019b66eb5f28e36c1f407e7bba92f3a0c5dc8d
Omri Vainkrantz <37619274+Omrizz@users.noreply.github.com>
2023-11-13 11:48:30 +0200

    Teraomri.tf
    spacing

--- Teraomri.tf
+++ Teraomri.tf
@@ -6,7 +6,6 @@ provider "aws" {
 # Create a Lambda role with CloudWatch logs policy
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -29,12 +28,9 @@ resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
 # Create a Lambda function with the Python code
 resource "aws_lambda_function" "github_webhook" {
   function_name = "github_webhook"
-
   role = aws_iam_role.lambda_role.arn
   handler = "lambda_function.lambda_handler"
-
   runtime = "python3.8"
-
   # Zip the Python code and upload it to Lambda
   filename = "${path.module}/lambda_function.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
@@ -72,20 +68,18 @@ resource "aws_api_gateway_deployment" "github_deployment" {
   depends_on = [
     aws_api_gateway_integration.github_integration,
   ]
-
   rest_api_id  = aws_api_gateway_rest_api.github_api.id
   stage_name = "prod"
 }
 
 # Create a GitHub provider with your GitHub token
 provider "github" {
-  token = "G1377566GY656!" # GitHub token
+  token = "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" # GitHub token
 }
 
 # Create a GitHub repository webhook to trigger the API Gateway endpoint
 resource "github_repository_webhook" "github_webhook" {
   repository = "7757574874" # Repository name
-
   configuration {
     url = aws_api_gateway_deployment.github_deployment.invoke_url # The API Gateway endpoint URL
     content_type = "json"
